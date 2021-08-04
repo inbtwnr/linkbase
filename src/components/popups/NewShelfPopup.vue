c0b4fa5cba0ef32c4dfa5c753c79d3ec021ed06d
login <alexanderjuror@gmail.com>
2021-08-04 10:56:56 +0300

    new shelf popup

--- src/components/popups/NewShelfPopup.vue
+++ src/components/popups/NewShelfPopup.vue
@@ -7,20 +7,22 @@
       }"
       @submit.prevent="submit"
     >
-      <div class="popup-block__header-line">
+      <div class="new-shelf-popup-block__header-line">
         <p class="header-2">{{ newShelfHeader }}</p>
-        <div class="popup-close" @click="ToggleNewShelfPopup">Cancel</div>
+        <div class="new-shelf-popup-close" @click="ToggleNewShelfPopup">
+          Cancel
+        </div>
       </div>
-      <div class="input-block">
+      <div class="new-shelf-input-block">
         <p class="paragraph-secondary">{{ newShelfLabelMessage }}</p>
         <input
           type="text"
-          class="input-text-block"
+          class="new-shelf-input-text-block"
           :placeholder="newShelfInputPlaceHolder"
           v-model="categoryTitle"
         />
       </div>
-      <button type="submit" class="confirm-button">
+      <button type="submit" class="new-shelf-confirm-button">
         <p class="paragraph-secondary">Add</p>
       </button>
     </form>
@@ -54,7 +56,7 @@ export default {
 
 <style lang="scss" scope>
 @import "@/assets/styles/_fonts.scss";
-
+@import "@/assets/styles/popup styles/_new-shelf.scss";
 @import "@/assets/styles/_main.scss";
 @media screen and (min-device-width: 360px) and (max-device-width: 424px) {
 }
@@ -65,75 +67,23 @@ export default {
 @media screen and (min-device-width: 1024px) and (max-device-width: 1364px) {
 }
 @media screen and (min-device-width: 1365px) {
-  .popup-screen {
-    position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    z-index: 98;
-    background-color: rgba(0, 0, 0, 0.1);
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  .new-shelf-confirm-button {
+    padding: 2 * $module 3 * $module;
+    border-radius: $module;
   }
-  .confirm-button {
-    cursor: pointer;
-    background-color: #3283fe;
-    border: none;
-    color: white;
-    padding: 2 * $module 4 * $module;
-    border-radius: 3 * $module;
-    .paragraph-secondary {
-      margin: 0;
-    }
-    &:hover {
-      background-color: #1966d8;
-    }
+  .new-shelf-popup-block__header-line {
+    margin-bottom: 4 * $module;
   }
-  .popup-block {
-    background: #fff;
-    padding: 24px 38px;
-    border-radius: 4px;
-    box-shadow: 0px 12px 12px rgba(0, 0, 0, 0.12);
-    z-index: 99;
-  }
-  .empty {
-    display: none;
-  }
-  .popup-block__header-line {
-    display: flex;
-    justify-content: space-between;
-    flex-direction: row;
-    align-content: baseline;
-    margin-bottom: 2 * $module;
-    p {
-      margin: 0;
-    }
-  }
-  .popup-close {
-    cursor: pointer;
-    color: #3283fe;
-  }
-  .input-block {
-    margin-bottom: 8 * $module;
-    background: #fff;
-    display: flex;
-    flex-direction: column;
+  .new-shelf-input-block {
+    margin-bottom: 6 * $module;
     .paragraph-secondary {
       margin-bottom: 2 * $module;
-      color: #6d7188;
     }
-    .input-text-block {
+    .new-shelf-input-text-block {
       width: 325px;
       padding: 2 * $module 4 * $module;
-      box-shadow: none;
-      font-size: 14px;
-      border: 1px solid #dddee4;
+      font-size: 16px;
       border-radius: $module;
-      :active {
-        border-radius: $module;
-      }
     }
   }
 }
